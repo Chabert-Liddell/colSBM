bff46ce2a22df934688f13f5f6fc4bb4988facab
Louis Lacoste <louis.lacoste@hotmail.fr>
2023-02-14 17:07:48 +0100

    Adding tests for the new functions

--- tests/testthat/test-R6class-fitBipartiteSBMPop.R
+++ tests/testthat/test-R6class-fitBipartiteSBMPop.R
@@ -1,3 +1,8 @@
+# Sourcing all necessary files
+require("sbm")
+source("R/utils.R")
+source("R/R6class-fitBipartiteSBMPop.R")
+
 set.seed(1234)
 coupleOfNodesPerClasses <- c(10, 25)
 Q <- c(3, 6)
@@ -21,4 +26,62 @@ colWithNA$A <- lapply(
 fitCol <- fitBipartiteSBMPop$new(A = col$A, Q = Q)
 fitColWithNA <- fitBipartiteSBMPop$new(A = colWithNA$A, Q = Q)
 
-fitCol$optimize()
+# Clustering by row and cols separately
+fitCol.spectral_biclustering <- lapply(
+    seq_along(fitCol$A),
+    function(m) {
+        spectral_biclustering(X = fitCol$A[[m]], K = fitCol$Q)
+    }
+)
+
+fitCol.spectral_biclustering
+
+# Co-Clustering
+
+fitCol.spectral_coclustering <- lapply(
+    seq_along(fitCol$A),
+    function(m) {
+        spectral_coclustering(X = fitCol$A[[m]], K = fitCol$Q)
+    }
+)
+
+fitCol.spectral_coclustering
+
+eps <- 0.05
+# Unipartite test of generation
+# pi <- c(0.1, 0.6, 0.3)
+
+# alphaCompartiment <- matrix(
+#     c(
+#         0.8, eps, 0.5,
+#         eps, 0.5, eps,
+#         eps, eps, .8
+#     ),
+#     nrow = 3
+# )
+# adj1 <- generate_unipartite_network(n = 50, pi = pi, alpha = alphaCompartiment)
+# plotMyMatrix(adj1)
+# sbm1 <- estimateSimpleSBM(adj1, estimOptions = list(plot=F, verbosity=1))
+# plot(sbm1)
+
+# Bipartite test of generation
+
+nr <- 500
+nc <- 2000
+
+pir <- c(0.1, 0.2, 0.4, 0.3)
+pic <- c(0.7, 0.1, 0.05, 0.05, 0.1)
+
+alpha <- matrix(
+    c(
+        0.9, eps, eps, eps, eps,
+        eps, 0.9, eps, eps, eps,
+        eps, eps, 0.9, eps, eps,
+        eps, eps, eps, 0.9, eps
+    ),
+    nrow = 4, ncol = 5, byrow = TRUE
+)
+
+incid1 <- generate_bipartite_network(nr, nc, pir, pic, alpha)
+lbm1 <- estimateBipartiteSBM(incid1)
+plot(lbm1)
---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,41 @@
+if (!require("testthat")) install.packages("testthat")
+library("testthat")
+source("R/utils.R")
+# Testing the generate_bipartite_collection
+set.seed(1234)
+
+eps <- 0.05
+
+pir <- c(0.1, 0.5, 0.2, 0.2)
+pic <- c(1 / 3, 1 / 3, 1 / 3)
+
+alpha <- matrix(c(
+    0.7, 0.6, 0.6,
+    0.6, 0.5, eps,
+    0.6, eps, eps,
+    eps, eps, 0.9
+), nrow = 4, byrow = TRUE)
+
+M <- 5
+
+# With one nr and one nc
+nr <- 120
+nc <- 240
+
+firstCol <- generate_bipartite_collection(nr, nc, pir, pic, alpha, M)
+
+test_that("nc and nr are extended to match M", {
+    expect_equal(unique(lapply(firstCol, nrow)), list(nr))
+    expect_equal(unique(lapply(firstCol, ncol)), list(nc))
+})
+
+# With increasing nr and nc
+nr <- c(5, 10, 20, 40, 80)
+nc <- 2 * nr
+
+increasingCol <- generate_bipartite_collection(nr, nc, pir, pic, alpha, M)
+
+test_that("Generate bipartite collection with vectors for nc and nr", {
+    expect_equal(lapply(increasingCol, nrow), as.list(nr))
+    expect_equal(lapply(increasingCol, ncol), as.list(nc))
+})
