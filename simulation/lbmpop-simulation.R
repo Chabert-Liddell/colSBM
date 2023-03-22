6d7676f462e257569fe7db4e198f7706f2bad0bd
Louis Lacoste <louis.lacoste@hotmail.fr>
2023-03-22 16:21:37 +0100

    Adding files to perform simulations

---
+++ simulation/lbmpop-simulation.R
@@ -0,0 +1,57 @@
+# Sourcing all necessary files
+require("sbm", quietly = T)
+require("dplyr", quietly = T)
+require("tictoc", quietly = T)
+require("ggplot2", quietly = T)
+
+# require("visNetwork", quietly = T)
+# require("igraph", quietly = T)
+source("R/utils.R")
+source("R/R6class-fitBipartiteSBMPop.R")
+source("R/R6class-lbmpop.R")
+
+set.seed(1234)
+
+verbose <- TRUE
+test_alea <- TRUE
+
+eps <- 0.05
+M <- 3
+nr <- 100
+nc <- 250
+
+pir <- c(0.2, 0.8)
+pic <- c(0.2, 0.3, 0.5)
+
+Q <- c(length(pir), length(pic))
+
+alpha <- matrix(
+    c(
+        0.9, eps, eps,
+        eps, 0.8, eps
+    ), nrow = Q[1], ncol = Q[2], byrow = TRUE
+)
+
+bipartite_collection <- generate_bipartite_collection(nr, nc, pir, pic, alpha, M)
+
+# This is a list of the M incidence matrices
+bipartite_collection_incidence <- lapply(seq.int(M), function(m) {
+    bipartite_collection[[m]]$incidence_matrix
+})
+
+
+## Init given with exact membership
+
+Z <- lapply(seq.int(M), function(m) {
+    list(bipartite_collection[[m]]$row_clustering, bipartite_collection[[m]]$col_clustering)
+})
+
+
+mylbmpop <- lbmpop$new(
+    netlist = bipartite_collection_incidence,
+    global_opts = list(verbosity = 4, plot_details = 1)
+)
+mylbmpop$burn_in()
+old_BICL <- mylbmpop$best_fit$BICL
+mylbmpop$moving_window(mylbmpop$best_fit$Q)
+mylbmpop$state_space_plot()
---
+++ simulation/play_lbmpop.R
@@ -0,0 +1,130 @@
+# Sourcing all necessary files
+require("sbm", quietly = T)
+require("dplyr", quietly = T)
+require("tictoc", quietly = T)
+require("ggplot2", quietly = T)
+
+# require("visNetwork", quietly = T)
+# require("igraph", quietly = T)
+source("R/utils.R")
+source("R/R6class-fitBipartiteSBMPop.R")
+source("R/R6class-lbmpop.R")
+
+set.seed(1234)
+eps <- 0.05
+M <- 3
+
+# Cas hote parasite
+# Modulaire : avec les diagonaux qui sont x3 et x4 des petites probas non diag
+
+nr_hp <- 1000
+nr_hp <- c(1 / 10 * nr_hp, 1 / 5 * nr_hp, nr_hp)
+nc_hp <- 2500
+nc_hp <- c(1 / 10 * nc_hp, 1 / 5 * nc_hp, nc_hp)
+
+pir_hp <- c(0.4, 0.3, 0.2, 0.05, 0.05)
+pic_hp <- c(0.45, 0.3, 0.2, 0.05)
+
+Q_hp <- c(length(pir_hp), length(pic_hp))
+
+alpha_hp <- matrix(
+    c(
+        15*eps, eps, eps, eps,
+        eps, 10*eps, eps, eps,
+        eps, eps, 15*eps, eps,
+        eps, eps, eps, 15*eps,
+        eps, eps, eps, eps
+    ),
+    nrow = Q_hp[1], ncol = Q_hp[2], byrow = TRUE
+)
+
+hp_bipartite_collection <- generate_bipartite_collection(nr_hp, nc_hp, pir_hp, pic_hp, alpha_hp, M)
+
+hp_bipartite_collection_incidence <- lapply(seq.int(M), function(m) {
+    hp_bipartite_collection[[m]]$incidence_matrix
+})
+
+### Cas plante-pollinisateurs double emboitements avec (1,3) partagé entre les emboîtements
+nr_pp <- 1000
+nr_pp <- c(1/10*nr_pp, 1/5*nr_pp, nr_pp)
+nc_pp <- 2500
+nc_pp <- c(1/10*nc_pp, 1/5*nc_pp, nc_pp)
+
+pir_pp <- c(0.4, 0.3, 0.2, 0.1)
+pic_pp <- c(0.45, 0.3, 0.2, 0.05)
+
+Q_pp <- c(length(pir_pp), length(pic_pp))
+
+alpha_pp <- matrix(
+    c(
+        15*eps, 10*eps, 10*eps, eps,
+        10*eps, eps, eps, eps,
+        eps, eps, 15*eps, 10*eps,
+        eps, eps, 10*eps, eps
+    ),
+    nrow = Q_pp[1], ncol = Q_pp[2], byrow = TRUE
+)
+
+pp_bipartite_collection <- generate_bipartite_collection(nr_pp, nc_pp, pir_pp, pic_pp, alpha_pp, M)
+
+pp_bipartite_collection_incidence <- lapply(seq.int(M), function(m) {
+    pp_bipartite_collection[[m]]$incidence_matrix
+})
+
+# La fusion des deux précédents
+
+bipartite_collection <- append(hp_bipartite_collection, pp_bipartite_collection)
+# This is a list of the M incidence matrices
+bipartite_collection_incidence <- lapply(seq_along(bipartite_collection), function(m) {
+    bipartite_collection[[m]]$incidence_matrix
+})
+
+
+## Init given with exact membership
+
+Z <- lapply(seq_along(bipartite_collection), function(m) {
+    list(
+        bipartite_collection[[m]]$row_clustering,
+        bipartite_collection[[m]]$col_clustering
+    )
+})
+
+
+# # mylbmpop$burn_in()
+# # mylbmpop$moving_window(mylbmpop$best_fit$Q)
+# # mylbmpop$compute_sep_LBM_ICL()
+# mylbmpop$optimize()
+
+# colLBM models
+cat("\n Hote-Parasite (Modulaire) :\n")
+
+hp_lbmpop <- lbmpop$new(
+    netlist = hp_bipartite_collection_incidence,
+    free_density = FALSE,
+    free_mixture = FALSE,
+    global_opts = list(verbosity = 4, plot_details = 1)
+)
+
+hp_lbmpop$optimize()
+
+cat("\n Plantes-Pollinisateurs (Double emboîté) :\n")
+
+pp_lbmpop <- lbmpop$new(
+    netlist = pp_bipartite_collection_incidence,
+    free_density = FALSE,
+    free_mixture = FALSE,
+    global_opts = list(verbosity = 4, plot_details = 1)
+)
+
+pp_lbmpop$optimize()
+
+cat("\n La collection associant les deux précédentes:\n")
+
+bad_lbmpop <- lbmpop$new(
+    netlist = bipartite_collection_incidence,
+    free_density = FALSE,
+    free_mixture = FALSE,
+    global_opts = list(verbosity = 4, plot_details = 1)
+)
+
+bad_lbmpop$optimize()
