699cd8ecdb02231ebb153bd1cf07c79d3938f631
Louis Lacoste <louis.lacoste@hotmail.fr>
2023-04-18 17:25:06 +0200

    working distance for bisbmpop

--- R/bmpop_distance.R
+++ R/bmpop_distance.R
@@ -85,3 +85,50 @@ dist_bmpop_max <- function(pi, alpha, delta = c(1,1), weight = "max",
 #
 # }
 #
+
+#' Compute the dissimilarity between 2 mesoscale structures
+#'
+#' @param pi A list of two probability vectors (row)
+#' @param rho A list of two probability vectors (columns)
+#' @param alpha A list of two connectivity matrices
+#' @param delta A vector of 2 density parameters (optional)
+#' @param weight One of "max" (default) or "mean". See details
+#' @param norm "L1"or "L2" norm for the computation
+#'
+#' @return The dissimilarity between two mesoscale structure.
+#'
+#' @details If weight is "max" then the weight of each block is computed as
+#'  \code{pmax(pi[[1]], pi[[2]])}. If "mean", then we take the average. "max"
+#'  penalize to a greater extent the difference in block proportion between
+#'  structure.
+#'
+#' @export
+#'
+#' @examples
+dist_bisbmpop_max <- function(pi, 
+rho, alpha, delta = c(1, 1), weight = "max", norm = "L2") {
+  if (missing(pi) || missing(rho)) {
+    distance <- switch(norm,
+      "L1" = sum(abs(alpha[[1]] / delta[1] - alpha[[2]] / delta[2])),
+      "L2" = sum((alpha[[1]] / delta[1] - alpha[[2]] / delta[2])**2)
+    )
+  } else {
+    w_pi <- switch(weight,
+      "max" = pmax(pi[[1]], pi[[2]]),
+      "mean" = .5 * (pi[[1]] + pi[[2]])
+    )
+    w_rho <- switch(weight,
+      "max" = pmax(rho[[1]], rho[[2]]),
+      "mean" = .5 * (rho[[1]] + rho[[2]])
+    )
+    distance <- switch(norm,
+      "L1" = sum(w_pi %*%
+        (abs(alpha[[1]] / delta[1] - alpha[[2]] / delta[2])) %*%
+        t(w_rho)),
+      "L2" = sum(w_pi %*%
+        ((alpha[[1]] / delta[1] - alpha[[2]] / delta[2])**2) %*%
+        t(w_rho))
+    )
+  }
+  distance
+}
