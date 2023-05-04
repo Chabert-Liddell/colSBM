9174d66f4738789d8bd3c759b3b51d15abe1595a
Louis Lacoste <louis.lacoste@hotmail.fr>
2023-05-04 14:27:50 +0200

    Testing with more networks non common pirho

--- simulation/lbmpop-simulation-pirho-empty-non-common.R
+++ simulation/lbmpop-simulation-pirho-empty-non-common.R
@@ -12,11 +12,13 @@ eps <- 0.05
 nr <- 250
 nc <- 250
 
-pir1 <- c(0.2, 0, 0.8)
+pir1 <- c(0.4, 0, 0.6)
 pir2 <- c(0.4, 0.6, 0)
+pir3 <- c(0, 0.4, 0.6)
 
 pic1 <- c(0.6, 0, 0.4)
-pic2 <- c(0.4, 0.6, 0)
+pic2 <- c(0.6, 0.4, 0)
+pic3 <- c(0, 0.6, 0.4)
 
 Q <- c(length(pir1), length(pic1))
 
@@ -32,7 +34,12 @@ alpha <- matrix(
 
 bipartite_collection <- list(
     generate_bipartite_network(nr, nc, pir1, pic1, alpha),
-    generate_bipartite_network(nr, nc, pir2, pic2, alpha)
+    generate_bipartite_network(nr, nc, pir2, pic2, alpha),
+    generate_bipartite_network(nr, nc, pir3, pic3, alpha),
+    generate_bipartite_network(nr, nc, pir2, pic3, alpha),
+    generate_bipartite_network(nr, nc, pir3, pic2, alpha),
+    generate_bipartite_network(nr, nc, pir1, pic2, alpha),
+    generate_bipartite_network(nr, nc, pir2, pic1, alpha)
 )
 
 M <- length(bipartite_collection)
@@ -49,7 +56,33 @@ Z <- lapply(seq.int(M), function(m) {
     list(bipartite_collection[[m]]$row_clustering, bipartite_collection[[m]]$col_clustering)
 })
 
-pi <- list(list(pir1, pic1), list(pir2, pic2))
+row_clusterings <- lapply(seq_along(bipartite_collection), function(m) {
+    return(bipartite_collection[[m]]$row_clustering)
+})
+
+col_clusterings <- lapply(seq_along(bipartite_collection), function(m) {
+    return(bipartite_collection[[m]]$col_clustering)
+})
+
+full_row_clustering <- as.vector(sapply(
+    seq.int(M),
+    function(m) row_clusterings[[m]]
+))
+
+full_col_clustering <- as.vector(sapply(
+    seq.int(M),
+    function(m) col_clusterings[[m]]
+))
+
+pi <- list(
+    list(pir1, pic1),
+    list(pir2, pic2),
+    list(pir3, pic3),
+    list(pir2, pic3),
+    list(pir3, pic2),
+    list(pir1, pic2),
+    list(pir2, pic1)
+)
 
 Cpi <- vector(mode = "list", length = 2)
 Cpi[[1]] <- vapply(seq(M), function(m) {
@@ -109,27 +142,34 @@ mybisbmpop3 <- estimate_colBiSBM(
     )
 )
 
-pirho_best_fit <- mybisbmpop3$best_fit$clone()
-pirho_best_fit$MAP$Z <- Z
-pirho_best_fit$MAP$pi <- pi
-pirho_best_fit$Cpi <- Cpi
-pirho_best_fit$Calpha <- Calpha
-pirho_best_fit$compute_vbound(MAP = TRUE)
-pirho_best_fit$compute_penalty(MAP = TRUE)
-pirho_best_fit$compute_BICL(MAP = TRUE)
+compute_double_ARI <- function(model) {
+    model_row_Z <- as.vector(sapply(
+        seq.int(model$M),
+        function(m) model$Z[[m]][[1]]
+    ))
 
+    model_col_Z <- as.vector(sapply(
+        seq.int(model$M),
+        function(m) model$Z[[m]][[2]]
+    ))
+
+    return(list(
+        aricode::ARI(model_row_Z, full_row_clustering),
+        aricode::ARI(model_col_Z, full_col_clustering)
+    ))
+}
 
 ari_sums <- sapply(
-    seq_along(mybisbmpop$best_fit$Z),
+    seq_along(mybisbmpop3$best_fit$Z),
     function(m) {
         c(
             aricode::ARI(
                 Z[[m]][[1]],
-                mybisbmpop$best_fit$Z[[m]][[1]]
+                mybisbmpop3$best_fit$Z[[m]][[1]]
             ),
             aricode::ARI(
                 Z[[m]][[2]],
-                mybisbmpop$best_fit$Z[[m]][[2]]
+                mybisbmpop3$best_fit$Z[[m]][[2]]
             )
         )
     }
