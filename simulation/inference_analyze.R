966199d9723443dc43b9396a440b0bac7a58d851
Louis Lacoste <louis.lacoste@hotmail.fr>
2023-05-09 16:59:52 +0200

    Added analyze for inference file

--- simulation/inference_analyze.R
+++ simulation/inference_analyze.R
@@ -1,1 +1,9 @@
-results <- readRDS("simulation/data/inference_testing_2023-04-26 16:31:55.Rds")
+filenames <- list.files(
+    path = "./simulation/data/",
+    pattern = "inference_testing_2023-05*",
+    full.names = TRUE
+)
+col_id_BICLS <- c(11, 16, 23, 30, 37)
+data_list <- lapply(filenames, readRDS)
+result_data_frame <- dplyr::bind_rows(data_list)
+result_data_frame <- cbind(result_data_frame, preferred_model = sapply(seq_len(nrow(result_data_frame)), function(n) names(which.max(result_data_frame[n, col_id_BICLS]))))
