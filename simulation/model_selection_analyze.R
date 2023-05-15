1f5f0e0795319bbd8fa4b49ad9cdc3831dd6e53d
Louis Lacoste <louis.lacoste@hotmail.fr>
2023-05-15 16:32:53 +0200

    Renaming and analyzing

--- simulation/model_selection_analyze.R
+++ simulation/model_selection_analyze.R
@@ -1,6 +1,7 @@
+require("ggplot2")
 filenames <- list.files(
     path = "./simulation/data/",
-    pattern = "model_selection_check*",
+    pattern = "model_selection_check_3_rep_f",
     full.names = TRUE
 )
 
@@ -8,10 +9,10 @@ data_list <- lapply(filenames, readRDS)
 result_data_frame <- dplyr::bind_rows(data_list)
 
 ggplot(data = result_data_frame) +
-    aes(x = epsilon_alpha, group = preferred_model, fill = preferred_model) +
+    aes(x = epsilon_pi, group = preferred_model, fill = preferred_model) +
     geom_bar()
 
 ggplot(data = result_data_frame) +
-    aes(x = , group = preferred_model, fill = preferred_model) +
+    aes(x = epsilon_rho , group = preferred_model, fill = preferred_model) +
     geom_bar()
 
