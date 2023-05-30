9f703e362ba3030e5bb7c764b84cdad6098d6306
Louis Lacoste <louis.lacoste@hotmail.fr>
2023-05-30 14:52:59 +0200

    Analyzing dore results

--- simulation/netclustering_dore_analyze.R
+++ simulation/netclustering_dore_analyze.R
@@ -3,6 +3,7 @@ require("tictoc")
 
 devtools::load_all("R/")
 
+# All results
 iid_clustering <- readRDS("simulation/data/dore_collection_clustering_nb_run1_iid_123networks_24-05-23-21:40:42.Rds")
 
 iid_best_partition <- extract_bipartite_best_partition(iid_clustering)
