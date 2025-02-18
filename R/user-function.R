a261681d626c2a3d87a103e29c7b91aba15ee756
Louis <louis.lacoste@agroparistech.fr>
2025-02-18 17:56:32 +0100

    Adding a first extract_nodes_groups

--- R/user-function.R
+++ R/user-function.R
@@ -534,3 +534,45 @@ adjust_colBiSBM <- function(
   adjusted_bisbmpop$adjusted_fit <- adjusted_bisbmpop$model_list[[Q[1], Q[2]]]
   return(adjusted_bisbmpop)
 }
+
+extract_nodes_groups <- function(fit, arg = rlang::caller_arg(fit)) {
+  if (!any(inherits(fit, c(
+    "fitSimpleSBMPop",
+    "fitBipartiteSBMPop",
+    "bmpop",
+    "bisbmpop"
+  )))) {
+    cli::cli_abort("{.var {arg}} must be a fitSimpleSBMPop, fitBipartiteSBMPop, bmpop or bisbmpop object.")
+  }
+  if (inherits(fit, "bmpop") || inherits(fit, "bisbmpop")) {
+    cli::cli_alert_info("As you've provided {.obj_type_friendly {fit}} and not {.obj_type_friendly {fit$best_fit}}, the function will extract from {.var {arg}$best_fit}.")
+    fit <- fit$best_fit
+  }
+
+
+  if (inherits(fit, "fitSimpleSBMPop")) {
+    cli::cli_text("Extracting nodes groups from a fitSimpleSBMPop object.")
+    fit$Z %>%
+      purrr::map_dfr(~ {
+        .x %>%
+          data.frame(node_name = names(.), cluster = .) %>%
+          rownames_to_column(var = "network")
+      }, .id = "network") -> out
+    return(out)
+  }
+  if (inherits(fit, "fitBipartiteSBMPop")) {
+    cli::cli_text("Extracting nodes groups from a fitBipartiteSBMPop object.")
+    fit$memberships %>%
+      purrr::map_dfr(~ {
+        row_df <- .x$row %>%
+          data.frame(node_name = names(.), cluster = ., node_type = "row") %>%
+          rownames_to_column(var = "network")
+
+        col_df <- .x$col %>%
+          data.frame(node_name = names(.), cluster = ., node_type = "col") %>%
+          rownames_to_column(var = "network")
+
+        dplyr::bind_rows(row_df, col_df)
+      }, .id = "network")
+  }
+}
